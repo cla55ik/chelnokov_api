667975715ffb0467b62a58ff5f7d183a6256d946
Ivan <ivan5420@yandex.ru>
2021-07-08 22:10:40 +0300

    create API functions methods

--- functions.php
+++ functions.php
@@ -1,1 +1,93 @@
 <?php
+
+
+function getProjects($connect){
+  $projects = mysqli_query($connect, "SELECT * FROM `projects`");
+  $projectsList = [];
+
+  
+  while($project = mysqli_fetch_assoc($projects)){
+    $projectsList[] = $project;
+  }
+  echo json_encode($projectsList);
+}
+
+
+function getProject($connect, $id){
+  $project = mysqli_query($connect, "SELECT * FROM `projects` WHERE `id` = '$id'");
+
+  if (mysqli_num_rows($project) === 0) {
+    http_response_code(404);
+    $res = [
+      "status" => false,
+      "message" => 'No found'
+    ];
+
+    echo json_encode($res);
+  }else{
+    $project = mysqli_fetch_assoc($project);
+
+    echo json_encode($project);
+  }
+
+
+}
+
+
+function addProject($connect, $data){
+  $title = $data['title'];
+  $description = $data['description'];
+  $img = $data['img'];
+  $stack = $data['stack'];
+  $url_site = $data['url_site'];
+  $url_git = $data['url_git'];
+
+
+
+  mysqli_query($connect, "INSERT INTO `projects`(`title`, `description`, `img`, `stack`, `url_site`, `url_git`) VALUES ('$title','$description','$img','$stack','$url_site','$url_git')");
+
+  http_response_code(201);
+
+  $res=[
+    "status"=>true,
+    "project_id" => mysqli_insert_id($connect)
+  ];
+
+  echo json_encode($res);
+
+}
+
+
+
+function updateProject($connect, $data, $id){
+  $title = $data['title'];
+  $description = $data['description'];
+  $img = $data['img'];
+  $stack = $data['stack'];
+  $url_site = $data['url_site'];
+  $url_git = $data['url_git'];
+
+  mysqli_query($connect, "UPDATE `projects` SET `title`='$title',`description`='$description',`img`='$img',`stack`='$stack',`url_site`='$url_site',`url_git`='$url_git' WHERE `projects`.`id` = '$id'");
+
+  http_response_code(200);
+
+  $res = [
+    "status" => true,
+    "message" => "Updated"
+  ];
+  echo json_encode($res);
+}
+
+
+function deleteProject($connect, $id){
+  mysqli_query($connect, "DELETE FROM `projects` WHERE `projects`.`id` = '$id'");
+
+  http_response_code(200);
+
+  $res = [
+    "status" => true,
+    "message" => 'Proj is delete'
+  ];
+
+  echo json_encode($res);
+}
--- index.php
+++ index.php
@@ -1,9 +1,51 @@
 <?php
 
+//die(print_r($_POST));
+
+header('Content-type: json/application');
+header('Access-Control-Allow-Origin: *');
+
 require 'connect.php';
-
-$projects = mysqli_query($connect, "SELECT * FROM `projects`");
-
-print_r($projects);
+require 'functions.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
+
+$q = $_GET['q'];
+$params = explode('/', $q);
+
+$type = $params[0];
+
+if (isset($params[1])) {
+  $id = $params[1];
+}
+//$id = $params[1];
+
+if ($method === 'GET') {
+  if ($type === 'projects') {
+
+    if (isset($id)) {
+      getProject($connect, $id);
+    }else{
+      getProjects($connect);
+    }
+
+  }
+}elseif ($method === 'POST') {
+  if ($type === 'projects') {
+    addProject($connect, $_POST);
+  }
+}elseif ($method === 'PATCH') {
+    if ($type === 'projects') {
+      if (isset($id)) {
+        $data = file_get_contents('php://input');
+        $data = json_decode($data, true);
+        updateProject($connect, $data, $id);
+      }
+    }
+}elseif ($method === 'DELETE') {
+  if ($type === 'projects') {
+    if(isset($id)){
+      deleteProject($connect, $id);
+    }
+  }
+}
